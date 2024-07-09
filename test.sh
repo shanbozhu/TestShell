d0137fa6357c041478ea2db479e50af03deea188
zhushanbo <zhushanbo@baidu.com>
2024-07-09 13:29:42 +0800

    no message

--- test.sh
+++ test.sh
@@ -3,9 +3,8 @@
 # ./mqrencode.sh "字符串"
 
 time=$(date "+%Y-%m-%d_%H-%M-%S")
-prefix="/Users/wsc/Desktop/mqrencode/"
+prefix="$HOME/Desktop/mqrencode/"
 suffix=".png"
 
-qrencode -o "${prefix}${time}${suffix}" -s 10 -m 1 "zhushanbo"
+qrencode -o "${prefix}${time}${suffix}" -s 10 -m 1 $1
 open ${prefix}
-echo "hello"
